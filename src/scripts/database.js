55d2efca9a4c3ba08350b23d5ed182a9df21dc19
Nathan Chevalier <cerberusgfx@gmail.com>
2023-07-31 15:58:08 -0500

    Database WIP

---
+++ src/scripts/database.js
@@ -0,0 +1,58 @@
+const database = {
+    band: [
+  { name: "The Cure", members: 4, genre: "Alternative Rock", formed: 1976 },
+  { name: "Depeche Mode", members: 3, genre: "Synthpop", formed: 1980 },
+  { name: "New Order", members: 3, genre: "Alternative Dance", formed: 1980 },
+  { name: "Duran Duran", members: 5, genre: "New Wave", formed: 1978 },
+  { name: "Tears for Fears", members: 2, genre: "New Wave", formed: 1981 },
+  { name: "The Smiths", members: 4, genre: "Indie Rock", formed: 1982 },
+  { name: "Bananarama", members: 3, genre: "Pop", formed: 1981 },
+  { name: "Pet Shop Boys", members: 2, genre: "Synthpop", formed: 1981 },
+  { name: "A-ha", members: 3, genre: "Synthpop", formed: 1982 },
+  { name: "Culture Club", members: 4, genre: "New Wave", formed: 1981 },
+  { name: "Spandau Ballet", members: 5, genre: "New Wave", formed: 1979 },
+  { name: "Talk Talk", members: 4, genre: "Synthpop", formed: 1981 }
+],
+    venue: [
+  {
+    name: "The Basement",
+    address: "1604 8th Ave S, Nashville, TN 37203",
+    sqfoot: 1200,
+    occupancy: 100
+  },
+  {
+    name: "The 5 Spot",
+    address: "1006 Forrest Ave, Nashville, TN 37206",
+    sqfoot: 1500,
+    occupancy: 125
+  },
+  {
+    name: "The High Watt",
+    address: "1 Cannery Row, Nashville, TN 37203",
+    sqfoot: 1100,
+    occupancy: 120
+  },
+  {
+    name: "Exit/In",
+    address: "2208 Elliston Pl, Nashville, TN 37203",
+    sqfoot: 1800,
+    occupancy: 150
+  },
+  {
+    name: "The East Room",
+    address: "2412 Gallatin Ave, Nashville, TN 37206",
+    sqfoot: 900,
+    occupancy: 75
+  },
+  {
+    name: "The End",
+    address: "2219 Elliston Pl, Nashville, TN 37203",
+    sqfoot: 800,
+    occupancy: 80
+  }
+],
+    booking:[
+    {}
+    ]
+
+}
